603c7be1576f7ac27397ae9598bf5067242c5652
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-10-14 23:51:49 -0300

    Detona Ralph em andamento

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,50 @@
-console.log('teste')
+/*
+// Gerenciamento de variaveis
+separamos em um objeto as variaveis por tipos que mudadm algumacoisa na tela
+das que rodam calculos por baixo dos panos
+views e values
+
+*/
+
+const state = {
+    view: {
+        squares: document.querySelectorAll('.square'),
+        enemy: document.querySelectorAll('.enemy'),
+        timeLeft: document.querySelectorAll('#time-left'),
+        score: document.querySelector('#score') 
+    },
+    values: {
+        timerId: null,
+        gameVelocit: 1000,
+    }
+};
+
+function randomSquare () {
+    
+    state.view.squares.forEach((square) => {
+        square.classList.remove('enemy');
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add('enemy')
+}
+
+function moveEnemy () {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocit)
+}
+
+function addListenerHitBox () {
+    state.view.squares.forEach((square) => {
+         
+    })
+
+}
+
+function initialize () {
+    moveEnemy()
+}
+
+
+initialize()
